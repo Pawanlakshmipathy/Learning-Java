f84a16ab068b88ba5c0632fd250b5a3f227bc68e
Pawan kumar <pawanlakshmipathy@gmail.com>
2025-05-06 20:08:32 +0530

    Add files via upload

---
+++ Strings/String to Integer (atoi).java
@@ -0,0 +1,32 @@
+class Solution {
+    public int myAtoi(String s) {
+        int i = 0, n = s.length(), sign = 1;
+        long result = 0;
+
+        while (i < n && s.charAt(i) == ' ') {
+            i++;
+        }
+
+        if (i < n && (s.charAt(i) == '+' || s.charAt(i) == '-')) {
+            sign = (s.charAt(i) == '-') ? -1 : 1;
+            i++;
+        }
+
+        // Step 3: Convert digits to integer
+        while (i < n && Character.isDigit(s.charAt(i))) {
+            result = result * 10 + (s.charAt(i) - '0');
+            i++;
+
+            // Step 4: Handle overflow
+            if (result * sign > Integer.MAX_VALUE) {
+                return Integer.MAX_VALUE;
+            }
+            if (result * sign < Integer.MIN_VALUE) {
+                return Integer.MIN_VALUE;
+            }
+        }
+
+        // Step 5: Return the result with the correct sign
+        return (int) (result * sign);
+    }
+}
